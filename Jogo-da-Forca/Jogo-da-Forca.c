723625614c07b6233f0a01645c3ab05687e5da83
Kaio Soledade <soledadekaio@gmail.com>
2023-08-21 22:50:32 -0300

    Update

--- Jogo-da-Forca/Jogo-da-Forca.c
+++ Jogo-da-Forca/Jogo-da-Forca.c
@@ -3,19 +3,36 @@
 
  int main(){
   int acertou = 0;
-  int enforcou = 1;
+  int enforcou = 0;
 
    char palavrasecreta[20];
    sprintf(palavrasecreta, "MELANCIA");
 
   char chutes[26];
   int tentativas = 0;
+
     do{
-      for(int i=0; i < strlen(palavrasecreta); i++){
-        printf("_ ");
+
+      for(int i = 0; i < strlen(palavrasecreta); i++){
+
+        int achou = 0;
+
+
+        for( int j = 0; j <= tentativas; j++ ){
+            if(chutes[j] == palavrasecreta[i]){
+            achou = 1;
+            break; 
+            }
+        }
+        if(achou){
+          printf("%c ", palavrasecreta[i]);
+        } else{
+          printf("_ ");
+        }
+
       }
       printf("\n");
-
+      
       char chute;
       scanf(" %c", &chute);
 
